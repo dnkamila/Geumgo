1db774d6b494aebbe859f752d0c092652e4200dd
kamila <dnkamila>
2016-09-29 08:21:21 +0900

    done minimal

--- app/src/main/java/com/idn/ict/geumgo/GridViewActivity.java
+++ app/src/main/java/com/idn/ict/geumgo/GridViewActivity.java
@@ -13,9 +13,13 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,9 +37,11 @@ public class GridViewActivity extends AppCompatActivity {
     private GridView gridView;
     private ProgressBar progressBar;
 
+    private String status = "0";
+
     private GridViewAdapter gridViewAdapter;
     private ArrayList<GridViewItem> gridViewItems;
-    private final String FEED_URL = "http://192.168.137.2/retrieve_log.php";
+    private final String RETRIEVE_LOG_URL = "http://192.168.137.2/retrieve_log.php";
     private final String UPDATE_STATUS_URL = "http://192.168.137.2/update_status.php";
     private final String RETRIEVE_STATUS_URL =  "http://192.168.137.2/retrieve_status.php";
 
@@ -52,6 +58,7 @@ public class GridViewActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "switch clicked");
+                new UpdateStatusTask().execute(UPDATE_STATUS_URL, status);
             }
         });
         
@@ -81,86 +88,19 @@ public class GridViewActivity extends AppCompatActivity {
             }
         });
 
-        new AsyncHttpTask().execute(FEED_URL);
-        progressBar.setVisibility(View.VISIBLE);
+        //new RetrieveStatusTask().execute(RETRIEVE_STATUS_URL);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        new RetrieveStatusTask().execute(RETRIEVE_STATUS_URL);
+        new RetrieveLogTask().execute(RETRIEVE_LOG_URL);
+        progressBar.setVisibility(View.VISIBLE);
     }
 
-    public class RetrieveStatusAsyncHttpTask extends AsyncTask<String, Void, Integer> {
-        @Override
-        protected Integer doInBackground(String... params) {
-            Integer result = 0;
-            try {
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
-                int statusCode = httpResponse.getStatusLine().getStatusCode();
-
-                if (statusCode == 200) {
-                    String response = streamToString(httpResponse.getEntity().getContent());
-                    parseResult(response);
-                    result = 1;
-                } else {
-                    result = 0;
-                }
-            } catch (Exception e) {
-                Log.d(TAG, e.getLocalizedMessage());
-            }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            if (result == 1) {
-                gridViewAdapter.setGridData(gridViewItems);
-            } else {
-                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
-            }
-
-            progressBar.setVisibility(View.GONE);
-        }
-    }
-
-    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
-        @Override
-        protected Integer doInBackground(String... params) {
-            Integer result = 0;
-            try {
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
-                int statusCode = httpResponse.getStatusLine().getStatusCode();
-
-                if (statusCode == 200) {
-                    String response = streamToString(httpResponse.getEntity().getContent());
-                    parseResult(response);
-                    result = 1;
-                } else {
-                    result = 0;
-                }
-            } catch (Exception e) {
-                Log.d(TAG, e.getLocalizedMessage());
-            }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            if (result == 1) {
-                gridViewAdapter.setGridData(gridViewItems);
-            } else {
-                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
-            }
-
-            progressBar.setVisibility(View.GONE);
-        }
-    }
-
-    String streamToString(InputStream stream) throws IOException {
+    private String streamToString(InputStream stream) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
         String line;
         String result = "";
@@ -174,13 +114,50 @@ public class GridViewActivity extends AppCompatActivity {
         return result;
     }
 
-    private void parseResult(String result) {
+    public class RetrieveLogTask extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    retrieveLogProcessing(response);
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if (result == 1) {
+                gridViewAdapter.setGridData(gridViewItems);
+            } else {
+                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    private void retrieveLogProcessing(String result) {
         try {
             JSONObject response = new JSONObject(result);
 
             JSONArray logs = response.getJSONArray("logs");
             int success = response.getInt("success");
             String message = response.getString("message");
+
+            gridViewItems.clear();
 
             if(success == 1) {
                 GridViewItem item;
@@ -203,5 +180,140 @@ public class GridViewActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public class RetrieveStatusTask extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            Log.d(TAG, "RETRIEVE doInBackground");
+
+            int result = 0;
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                Log.d("rab", "status code : " + statusCode);
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    retrieveStatusProcessing(response);
+                    result = 1;
+                    Log.d("rab","success : " + result);
+                } else {
+                    result = 0;
+                    Log.d("rab","fail : " + result);
+                }
+            } catch (Exception e) {
+                Log.d("rab", e.getLocalizedMessage());
+                Log.d("rab","exception : " + result);
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if (result == 1) {
+
+            } else {
+                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    private void retrieveStatusProcessing(String result) {
+        try {
+            JSONObject response = new JSONObject(result);
+
+            final String status = response.getString("status");
+            int success = response.getInt("success");
+            String message = response.getString("message");
+
+            if(success == 1) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setSwitch(status);
+                    }
+                });
+            }
+            else {
+                Toast.makeText(this.getBaseContext(), message, Toast.LENGTH_LONG).show();
+            }
+
+            Log.d("rab", "BERHASIL KE UPDATE");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setSwitch(String status) {
+        Log.d("rab", "toogle switch?");
+        if(status.equals("0")) {
+            this.status = status;
+            imageView.setImageResource(R.mipmap.ic_off);
+        }
+        else {
+            this.status = status;
+            imageView.setImageResource(R.mipmap.ic_on);
+        }
+        Log.d("rab", "toogle switch");
+    }
+
+    public class UpdateStatusTask extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            Log.d(TAG, "UPDATE doInBackground");
+            Integer result = 0;
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(params[0]);
+
+                ArrayList<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+                if(params[1].equals("0")) {
+                    nameValuePair.add(new BasicNameValuePair("status", "1"));
+                }
+                else {
+                    nameValuePair.add(new BasicNameValuePair("status", "0"));
+                }
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair, "UTF-8"));
+                HttpResponse httpResponse = httpclient.execute(httpPost);
+
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    updateStatusProcessing(response);
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if (result == 1) {
+
+            } else {
+                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+
+            progressBar.setVisibility(View.GONE);
+            Log.d(TAG, "status :" + (Integer.parseInt(status) == 0 ? 1 : 0));
+            setSwitch("" + (Integer.parseInt(status) == 0 ? 1 : 0));
+        }
+    }
+
+    private void updateStatusProcessing(String result) {
+        Log.d(TAG, "RETRIEVE after UPDATE");
+
+        //new RetrieveStatusTask().execute(RETRIEVE_STATUS_URL);
     }
 }
--- app/src/main/java/com/idn/ict/geumgo/GridViewAdapter.java
+++ app/src/main/java/com/idn/ict/geumgo/GridViewAdapter.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,9 +23,13 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,7 +41,7 @@ public class GridViewAdapter extends ArrayAdapter<GridViewItem> {
     private int layoutResourceId;
     private ArrayList<GridViewItem> mGridData = new ArrayList<GridViewItem>();
 
-    private String REMOVE_URL = "http://192.168.137.2/delete_log.php?";
+    private String DELETE_LOG_URL = "http://192.168.137.2/delete_log.php";
 
     public GridViewAdapter(Context mContext, int layoutResourceId, ArrayList<GridViewItem> mGridData) {
         super(mContext, layoutResourceId, mGridData);
@@ -51,13 +56,16 @@ public class GridViewAdapter extends ArrayAdapter<GridViewItem> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final GridViewItem item = mGridData.get(position);
+
         View row = convertView;
         ViewHolder holder;
 
         if (row == null) {
             LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
             row = inflater.inflate(layoutResourceId, parent, false);
+
             holder = new ViewHolder();
             holder.imageUrl = (ImageView) row.findViewById(R.id.grid_item_image_url);
             holder.capturedAt = (TextView) row.findViewById(R.id.grid_item_captured_at);
@@ -70,7 +78,27 @@ public class GridViewAdapter extends ArrayAdapter<GridViewItem> {
             holder = (ViewHolder) row.getTag();
         }
 
-        GridViewItem item = mGridData.get(position);
+        int temp = Integer.parseInt(String.valueOf(Html.fromHtml(item.getLevel())));
+        switch (temp) {
+            case 1:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    row.setBackground(getContext().getResources().getDrawable(R.drawable.grid_view_item_warning_one));
+                }
+                break;
+            case 2:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    row.setBackground(getContext().getResources().getDrawable(R.drawable.grid_view_item_warning_two));
+                }
+                break;
+            case 3:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    row.setBackground(getContext().getResources().getDrawable(R.drawable.grid_view_item_warning_three));
+                }
+                break;
+        }
+
+        row.setTag(holder);
+
         holder.capturedAt.setText(Html.fromHtml(item.getCapuredAt()));
         holder.level.setText(Html.fromHtml(item.getLevel()));
         holder.description.setText(Html.fromHtml(item.getDescription()));
@@ -78,12 +106,70 @@ public class GridViewAdapter extends ArrayAdapter<GridViewItem> {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "remove clicked");
-            }
+                new DeleteLogTask().execute(DELETE_LOG_URL, "" + item.getLogId());
+                mGridData.remove(position);
+                notifyDataSetChanged();
+                notifyDataSetInvalidated();
+           }
         });
 
         Picasso.with(mContext).load(item.getImageUrl()).into(holder.imageUrl);
 
         return row;
+    }
+
+    private String streamToString(InputStream stream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            result += line;
+        }
+
+        if (null != stream) {
+            stream.close();
+        }
+        return result;
+    }
+
+    public class DeleteLogTask extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            Log.d(TAG, "UPDATE doInBackground");
+            Integer result = 0;
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(params[0]);
+
+                ArrayList<NameValuePair> nameValuePair = new ArrayList<NameValuePair>();
+                nameValuePair.add(new BasicNameValuePair("log_id", params[1]));
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair, "UTF-8"));
+                HttpResponse httpResponse = httpclient.execute(httpPost);
+
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    deleteLogProcessing(response);
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+
+        }
+    }
+
+    private void deleteLogProcessing(String response) {
+        Log.d(TAG, response);
     }
 
     static class ViewHolder {
